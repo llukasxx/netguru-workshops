a89f30fc5a826b1facb66e00014a5330b1e4ad1f
Łukasz Gorzelany <llukasxx@gmail.com>
2015-06-11 13:12:39 +0200

    Add spec checking correct review user

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -7,4 +7,13 @@ describe Review do
     it { should validate_presence_of :user_id }
     it { should belong_to :user }
   end
+
+  describe 'assignments' do
+    let(:user)    { create(:user) }
+    let(:product)    { create(:product) }
+    it "has proper user" do
+      review = user.reviews.create(content: "Very bad thing", product_id: product.id)
+      expect(review.user_id).to eq user.id
+    end
+  end
 end
