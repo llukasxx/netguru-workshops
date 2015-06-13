81194e9303fbcb017a07a6afb62a8b95592b8005
Łukasz Gorzelany <llukasxx@gmail.com>
2015-06-13 14:07:59 +0200

    More styling and footer

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,6 @@ class Review < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :user_id, :content, :rating
+  validates :rating, inclusion: { in: 1..5, message: "The rating must be between 1 and 5" }
 
 end
